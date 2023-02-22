c3af29ce13ac75f8f89d014c746d69e8902ff629
4UR3L10 <aureliomartinezdros@outlook.com>
2023-02-21 23:26:11 -0500

    Login Form
    +Added a login form
    +Added the code to open the login form.

--- Aday-DA/Aday-DA/Form_AddPlan.cs
+++ Aday-DA/Aday-DA/Form_AddPlan.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using Aday_DA.Classes;
 
 namespace Aday_DA
 {
---
+++ Aday-DA/Aday-DA/Form_Login.Designer.cs
@@ -0,0 +1,229 @@
+﻿
+namespace Aday_DA
+{
+    partial class Form_Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_Login));
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelHeader = new System.Windows.Forms.Label();
+            this.PictureBoxUsername = new System.Windows.Forms.PictureBox();
+            this.panelUsername = new System.Windows.Forms.Panel();
+            this.panelPassword = new System.Windows.Forms.Panel();
+            this.PictureBoxPassword = new System.Windows.Forms.PictureBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnSignUp = new System.Windows.Forms.Button();
+            this.lblForgotPassword = new System.Windows.Forms.Label();
+            this.labelSignOut = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxUsername)).BeginInit();
+            this.panelUsername.SuspendLayout();
+            this.panelPassword.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxPassword)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(235)))), ((int)(((byte)(235)))));
+            this.textBoxUsername.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxUsername.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBoxUsername.Location = new System.Drawing.Point(49, 11);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(229, 18);
+            this.textBoxUsername.TabIndex = 2;
+            this.textBoxUsername.Click += new System.EventHandler(this.textBoxUsername_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(152)))), ((int)(((byte)(221)))));
+            this.panel1.Controls.Add(this.labelHeader);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(489, 74);
+            this.panel1.TabIndex = 4;
+            // 
+            // labelHeader
+            // 
+            this.labelHeader.AutoSize = true;
+            this.labelHeader.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelHeader.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.labelHeader.Location = new System.Drawing.Point(128, 18);
+            this.labelHeader.Name = "labelHeader";
+            this.labelHeader.Size = new System.Drawing.Size(252, 37);
+            this.labelHeader.TabIndex = 5;
+            this.labelHeader.Text = "ADAY USER LOGIN";
+            // 
+            // PictureBoxUsername
+            // 
+            this.PictureBoxUsername.BackColor = System.Drawing.SystemColors.Control;
+            this.PictureBoxUsername.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.PictureBoxUsername.Cursor = System.Windows.Forms.Cursors.Default;
+            this.PictureBoxUsername.Image = global::Aday_DA.Properties.Resources.clipart836353;
+            this.PictureBoxUsername.Location = new System.Drawing.Point(0, 0);
+            this.PictureBoxUsername.Name = "PictureBoxUsername";
+            this.PictureBoxUsername.Size = new System.Drawing.Size(34, 38);
+            this.PictureBoxUsername.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PictureBoxUsername.TabIndex = 5;
+            this.PictureBoxUsername.TabStop = false;
+            // 
+            // panelUsername
+            // 
+            this.panelUsername.Controls.Add(this.PictureBoxUsername);
+            this.panelUsername.Controls.Add(this.textBoxUsername);
+            this.panelUsername.Location = new System.Drawing.Point(103, 158);
+            this.panelUsername.Name = "panelUsername";
+            this.panelUsername.Size = new System.Drawing.Size(292, 38);
+            this.panelUsername.TabIndex = 6;
+            // 
+            // panelPassword
+            // 
+            this.panelPassword.Controls.Add(this.PictureBoxPassword);
+            this.panelPassword.Controls.Add(this.textBoxPassword);
+            this.panelPassword.Location = new System.Drawing.Point(103, 217);
+            this.panelPassword.Name = "panelPassword";
+            this.panelPassword.Size = new System.Drawing.Size(292, 38);
+            this.panelPassword.TabIndex = 7;
+            // 
+            // PictureBoxPassword
+            // 
+            this.PictureBoxPassword.BackColor = System.Drawing.SystemColors.Control;
+            this.PictureBoxPassword.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.PictureBoxPassword.Cursor = System.Windows.Forms.Cursors.Default;
+            this.PictureBoxPassword.Image = ((System.Drawing.Image)(resources.GetObject("PictureBoxPassword.Image")));
+            this.PictureBoxPassword.Location = new System.Drawing.Point(0, 0);
+            this.PictureBoxPassword.Name = "PictureBoxPassword";
+            this.PictureBoxPassword.Size = new System.Drawing.Size(34, 38);
+            this.PictureBoxPassword.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PictureBoxPassword.TabIndex = 5;
+            this.PictureBoxPassword.TabStop = false;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(235)))), ((int)(((byte)(235)))));
+            this.textBoxPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxPassword.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBoxPassword.Location = new System.Drawing.Point(49, 11);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(229, 18);
+            this.textBoxPassword.TabIndex = 3;
+            this.textBoxPassword.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBoxPassword_MouseClick);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnLogin.FlatAppearance.BorderSize = 0;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnLogin.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnLogin.Location = new System.Drawing.Point(152, 306);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(209, 36);
+            this.btnLogin.TabIndex = 1;
+            this.btnLogin.Text = "LOGIN";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            // 
+            // btnSignUp
+            // 
+            this.btnSignUp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(152)))), ((int)(((byte)(221)))));
+            this.btnSignUp.FlatAppearance.BorderSize = 0;
+            this.btnSignUp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSignUp.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSignUp.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnSignUp.Location = new System.Drawing.Point(152, 357);
+            this.btnSignUp.Name = "btnSignUp";
+            this.btnSignUp.Size = new System.Drawing.Size(209, 36);
+            this.btnSignUp.TabIndex = 4;
+            this.btnSignUp.Text = "Sign Up";
+            this.btnSignUp.UseVisualStyleBackColor = false;
+            // 
+            // lblForgotPassword
+            // 
+            this.lblForgotPassword.AutoSize = true;
+            this.lblForgotPassword.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblForgotPassword.Location = new System.Drawing.Point(103, 272);
+            this.lblForgotPassword.Name = "lblForgotPassword";
+            this.lblForgotPassword.Size = new System.Drawing.Size(103, 15);
+            this.lblForgotPassword.TabIndex = 5;
+            this.lblForgotPassword.Text = "Forgot Password ?";
+            // 
+            // labelSignOut
+            // 
+            this.labelSignOut.AutoSize = true;
+            this.labelSignOut.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.labelSignOut.Location = new System.Drawing.Point(342, 272);
+            this.labelSignOut.Name = "labelSignOut";
+            this.labelSignOut.Size = new System.Drawing.Size(53, 15);
+            this.labelSignOut.TabIndex = 6;
+            this.labelSignOut.Text = "Sign Out";
+            // 
+            // Form_Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 450);
+            this.Controls.Add(this.labelSignOut);
+            this.Controls.Add(this.lblForgotPassword);
+            this.Controls.Add(this.btnSignUp);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.panelPassword);
+            this.Controls.Add(this.panelUsername);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form_Login";
+            this.Text = "Aday Login";
+            this.Load += new System.EventHandler(this.Form_Login_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxUsername)).EndInit();
+            this.panelUsername.ResumeLayout(false);
+            this.panelUsername.PerformLayout();
+            this.panelPassword.ResumeLayout(false);
+            this.panelPassword.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxPassword)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label labelHeader;
+        private System.Windows.Forms.PictureBox PictureBoxUsername;
+        private System.Windows.Forms.Panel panelUsername;
+        private System.Windows.Forms.Panel panelPassword;
+        private System.Windows.Forms.PictureBox PictureBoxPassword;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnSignUp;
+        private System.Windows.Forms.Label lblForgotPassword;
+        private System.Windows.Forms.Label labelSignOut;
+    }
+}
