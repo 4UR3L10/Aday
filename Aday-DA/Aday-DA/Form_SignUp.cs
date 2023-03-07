992283a1e39ac5600110d95c663f34457f4fe6e1
4UR3L10 <aureliomartinezdros@outlook.com>
2023-03-06 23:22:16 -0500

    SignUpForm

---
+++ Aday-DA/Aday-DA/Form_SignUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Aday_DA.Resources
+{
+    public partial class Form_SignUp : Form
+    {
+        public Form_SignUp()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_SignUp_Load(object sender, EventArgs e)
+        {
+            textBoxPassword.Text = "Type Your Password";
+            textBoxUsername.Text = "Type Your Username";
+        }
+    }
+}
